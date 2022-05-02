834eedeef17df5e2f5e1a908339e5013e9cd8b18
Michael/Misha Khmelkov <mishakhm@gmail.com>
2022-05-02 15:52:38 -0700

    Very basic "my classes" functionality + improvements

--- core.js
+++ core.js
@@ -53,84 +53,91 @@ function showSearchedCourses(searchedCourseList, dept){
     for(let i = 0; i<searchedCourseList.length; i++){
         //Check that the course actually belongs in this search (because USC's API will return all courses connected to a department even if they don't match the prefix, i.e. QBIO-401 will show up when searching for BISC courses. Webreg's own search only shows courses that match the prefix exactly)
         if(searchedCourseList[i].CourseData.prefix == dept){
-            //Create div for all info for a single course
-            var searchedCourse = document.createElement("div");
-            searchedCourse.className = "searchResult";
-
-            //Find how many units the course is worth
-            var units = "0.0";
-            for(let j = 0; j<searchedCourseList[i].CourseData.SectionData.length; j++){
-                if(parseFloat(searchedCourseList[i].CourseData.SectionData[j].units) > parseFloat(units)){
-                    units = searchedCourseList[i].CourseData.SectionData[j].units;
-                }
-            }
-
-            //Create button with outer info
-            button = addElement("button", "searchResultButton", searchedCourse, "");
-            addElement("p", "", button, 
-            searchedCourseList[i].CourseData.prefix
-            + "-" + searchedCourseList[i].CourseData.number
-            + ": " + searchedCourseList[i].CourseData.title);
-            addElement("p", "", button, "Units: " + units);
-            
-            //Add course info
-            var searchedCourseInfo = document.createElement("div");
-            searchedCourseInfo.className = "searchedCourseInfo";
-            //Add pre-req and co-req info
-            if (typeof searchedCourseList[i].CourseData.prereq_text == "string"){
-                addElement("div", "", searchedCourseInfo,
-                "Pre-reqs: " + searchedCourseList[i].CourseData.prereq_text)}
-            if (typeof searchedCourseList[i].CourseData.coreq_text == "string"){
-                addElement("div", "", searchedCourseInfo,
-                "Co-reqs: " + searchedCourseList[i].CourseData.coreq_text)}
-            //Add all course sections to info
-            for(let j = 0; j<searchedCourseList[i].CourseData.SectionData.length; j++){
-                var section = document.createElement("div");
-                section.className = "searchSection";
-                
-                addElement("p", "", section, 
-                searchedCourseList[i].CourseData.SectionData[j].id
-                + " " + searchedCourseList[i].CourseData.SectionData[j].dclass_code
-                + ": " + searchedCourseList[i].CourseData.SectionData[j].type);
-
-                addElement("p", "", section, 
-                searchedCourseList[i].CourseData.SectionData[j].day
-                + " " + searchedCourseList[i].CourseData.SectionData[j].start_time
-                + "-" + searchedCourseList[i].CourseData.SectionData[j].end_time);
-
-                //Check if an instructor is listed for the course, and either display their name or a message
-                if (typeof searchedCourseList[i].CourseData.SectionData[j].instructor != "undefined"){
-                    if(Array.isArray(searchedCourseList[i].CourseData.SectionData[j].instructor)){
-                        for(let k = 0; k<searchedCourseList[i].CourseData.SectionData[j].instructor.length;k++){
-                            addElement("p", "", section, 
-                            searchedCourseList[i].CourseData.SectionData[j].instructor[k].last_name
-                            + ", " + searchedCourseList[i].CourseData.SectionData[j].instructor[k].first_name);}
-                    }
-                    else{
-                        addElement("p", "", section, 
-                        searchedCourseList[i].CourseData.SectionData[j].instructor.last_name
-                        + ", " + searchedCourseList[i].CourseData.SectionData[j].instructor.first_name);}}
-                else{addElement("p", "", section, "No instructor listed");}
-
-                addElement("p", "", section, 
-                searchedCourseList[i].CourseData.SectionData[j].number_registered
-                + "/" + searchedCourseList[i].CourseData.SectionData[j].spaces_available);
-
-                searchedCourseInfo.appendChild(section);
-            }
-            searchedCourse.appendChild(searchedCourseInfo);
-            //Add the whole course div to the results
-            document.getElementById("searchResultsContainer").appendChild(searchedCourse);
+            createClassHTML(searchedCourseList[i], document.getElementById("searchResultsContainer"), "search");
         }
-    }
-    //Accordion dropdown function for revealing section info
-    var accordion = document.getElementsByClassName("searchResultButton");
-    for (i=0; i<accordion.length; i++) {
-        accordion[i].addEventListener('click', function () {
-            toggleShow(this.nextElementSibling);})
     }
     //Hide loading indicator now that search has successfully completed
     document.getElementById("loading").style.display = "none";
+}
+
+function createClassHTML(course, addTo, type){
+    //Create div for all info for a single course
+    var displayCourse = document.createElement("div");
+    displayCourse.className = "searchResult";
+
+    //Find how many units the course is worth
+    var units = "0.0";
+    for(let j = 0; j<course.CourseData.SectionData.length; j++){
+        if(parseFloat(course.CourseData.SectionData[j].units) > parseFloat(units)){
+            units = course.CourseData.SectionData[j].units;
+        }
+    }
+
+    //Create button with outer info
+    button = addElement("button", "searchResultButton", displayCourse, "");
+    addElement("p", "", button, 
+    course.CourseData.prefix
+    + "-" + course.CourseData.number
+    + ": " + course.CourseData.title);
+    addElement("p", "", button, "Units: " + units);
+    
+    //Add course info
+    var displayCourseInfo = document.createElement("div");
+    displayCourseInfo.className = "displayCourseInfo";
+    //"Add class" button if in search
+    if (type == "search"){
+        addClassButton = addElement("button", "addClass", displayCourseInfo, "Add class to my list");
+        addClassButton.addEventListener('click', function () {
+            addClass(course);
+        });}
+    //Add pre-req and co-req info
+    if (typeof course.CourseData.prereq_text == "string"){
+        addElement("div", "", displayCourseInfo,
+        "Pre-reqs: " + course.CourseData.prereq_text)}
+    if (typeof course.CourseData.coreq_text == "string"){
+        addElement("div", "", displayCourseInfo,
+        "Co-reqs: " + course.CourseData.coreq_text)}
+    //Add all course sections to info
+    for(let j = 0; j<course.CourseData.SectionData.length; j++){
+        var section = document.createElement("div");
+        section.className = "searchSection";
+        
+        addElement("p", "", section, 
+        course.CourseData.SectionData[j].id
+        + " " + course.CourseData.SectionData[j].dclass_code
+        + ": " + course.CourseData.SectionData[j].type);
+
+        addElement("p", "", section, 
+        course.CourseData.SectionData[j].day
+        + " " + course.CourseData.SectionData[j].start_time
+        + "-" + course.CourseData.SectionData[j].end_time);
+
+        //Check if an instructor is listed for the course, and either display their name or a message
+        if (typeof course.CourseData.SectionData[j].instructor != "undefined"){
+            if(Array.isArray(course.CourseData.SectionData[j].instructor)){
+                for(let k = 0; k<course.CourseData.SectionData[j].instructor.length;k++){
+                    addElement("p", "", section, 
+                    course.CourseData.SectionData[j].instructor[k].last_name
+                    + ", " + course.CourseData.SectionData[j].instructor[k].first_name);}
+            }
+            else{
+                addElement("p", "", section, 
+                course.CourseData.SectionData[j].instructor.last_name
+                + ", " + course.CourseData.SectionData[j].instructor.first_name);}}
+        else{addElement("p", "", section, "No instructor listed");}
+
+        addElement("p", "", section, 
+        course.CourseData.SectionData[j].number_registered
+        + "/" + course.CourseData.SectionData[j].spaces_available);
+
+        displayCourseInfo.appendChild(section);
+    }
+    displayCourse.appendChild(displayCourseInfo);
+    //Add the whole course div to the results
+    addTo.appendChild(displayCourse);
+    //Add accordion dropdown functionality
+    button.addEventListener('click', function () {
+        toggleShow(this.nextElementSibling);})
 }
 
 function toggleShow(element){
@@ -139,14 +146,44 @@ function toggleShow(element){
     else {element.style.display = "block";}
 }
 
+function addClass(course){
+    //Need to add a check that the course isn't already in list
+    classes.push(course);
+    //Save updated list of classes - using storage.sync currently exceeds QUOTA_BYTES_PER_ITEM but would eventually be ideal
+    chrome.storage.local.set({'classes': classes});
+    //Show updated list of classes
+    showMyClasses();
+}
 
-var term = "20223";
+function clearClasses(){
+    classes = [];
+    chrome.storage.local.set({'classes': classes});
+    showMyClasses();
+}
+
+function showMyClasses(){
+    //Clear classes div
+    document.getElementById("myClassesContainer").innerHTML = "";
+    for(i=0; i<classes.length; i++){
+        createClassHTML(classes[i], document.getElementById("myClassesContainer"), "myClasses");
+    }
+}
+
+//chrome.storage.local.set({'term': "20223"})
+var term;
+var classes;
+chrome.storage.local.get(['term','classes'], data => {
+    term = data.term;
+    if (typeof data.classes != "undefined"){classes = data.classes}
+    else{classes = []}
+    showMyClasses();
+});
 
 //Adds search listener to dept search box
 var input = document.getElementById("input");
 input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-      event.preventDefault();
-      searchDept(event.target.value.toUpperCase(),term);
+        event.preventDefault();
+        searchDept(event.target.value.toUpperCase(),term);
     }
-  });
+});
