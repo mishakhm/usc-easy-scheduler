81ee2bb2ed92ae33625c55c00380490fe569114a
Michael/Misha Khmelkov <mishakhm@gmail.com>
2022-08-28 11:08:02 -0700

    Added opening on install and info slide on how to open the app in future

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
 chrome.action.onClicked.addListener(function() {
   chrome.tabs.create({url: chrome.runtime.getURL('calendar.html')});
 });
+
+chrome.runtime.onInstalled.addListener(function(object) {
+  if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    chrome.tabs.create({url: chrome.runtime.getURL('calendar.html')});
+  }
+});
